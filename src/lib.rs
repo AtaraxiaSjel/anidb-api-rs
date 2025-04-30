855ebad5a2cbc31236d464180600ac720184d166
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-04-30 12:16:41 +0300

    feat: add features for http and udp clients

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
+/*!
+# Example
+
+# Crate features
+
+*/
+#![allow(dead_code)]
+#[cfg(feature = "http")]
 /// `AniDB` HTTP Api definition: <https://wiki.anidb.net/HTTP_API_Definition>
 pub mod http;
+
+#[cfg(feature = "udp")]
+/// `AniDB` UDP Api definition: <https://wiki.anidb.net/UDP_API_Definition>
+pub mod udp;
